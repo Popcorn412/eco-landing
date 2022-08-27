15814be5d1cbad4368f12fa0ab82625ed9de3c70
popcorn <HighleyHilmaU@gmail.com>
2022-08-27 15:04:33 +0900

    add: all content

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import reportWebVitals from './reportWebVitals';
 
+import { slice } from './useStore';
+
+import App from './App';
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
+
+const store = configureStore({reducer: slice.reducer});
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
